74af1fe0f6ed5c2dbf7b6cadb031eb6157468c8e
GustavoAlves04 <gustavoalcontato@gmail.com>
2023-04-25 20:20:03 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const textarea = document.querySelector("textarea"),
-    voiceList = document.querySelector("select"),
-    speechBtn = document.querySelector("button");
+voiceList = document.querySelector("select"),
+speechBtn = document.querySelector("button");
 
 let synth = speechSynthesis,
-    isSpeaking = true;
+isSpeaking = true;
 
 voices();
 
 function voices() {
-  for (let voice of synth.getVoices()) {
-    const selected = voice.name === "Google US English" ? "selected" : "";
-    const option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option`;
-    voiceList.insertAdjacentHTML("beforeend", option);
-  }
+    for (let voice of synth.getVoices()) {
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text) {
+    let utterance = new SpeechSynthesisUtterance(text);
+    for (let voice of synth.getVoices()) {
+        if (voice.name === voiceList.value) {
+            utterance.voice = voice;
+        }
+    }
+    synth.speak(utterance);
 }
 
 speechBtn.addEventListener("click", (e) => {
